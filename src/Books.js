8d941dd73fc2976fb4b1551d1dff5fcad1d7dcf4
Hisstori <browncryan@outlook.com>
2020-03-06 14:27:26 -0500

    testing customization

--- src/Books.js
+++ src/Books.js
@@ -1,19 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
 
-const url = 'http://localhost:3280/';
+// const url = 'http://localhost:3280/';
 
-class Books extends Component {
-    state = {}
-
-    componentDidMount() {
-        const books = this.props.match.param.isbn;
-        const url = `${url}${Books}.json`
-        console.log(url)
-    }
-}
-    axios.get('hhttp://localhost:3280/books')
+// class Books extends Component {
+//     componentDidMount() {
+//         const books = this.props.match.param.isbn;
+//         const url = `${url}${Books}.json`
+//         console.log(url)
+//     }
+//}
+    axios.get('http://localhost:3000/books')
     .then(response => {
       console.log(response.data);
     })
