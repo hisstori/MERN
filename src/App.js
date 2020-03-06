ae363c98bcafbf21d620dcf9ae80d9f056518e47
Hisstori <browncryan@outlook.com>
2020-03-06 11:30:22 -0500

    Testing tailwind

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>
-        <form>
-          <label>
-            ==New Book Entry==
-            Title: <input type='text' title='title'/> 
-            isbn: <input type='text' isbn='isbn'/>
-            Page Count:  <input type='number' pageCount='Page Count'/>
-            Description: <input type='text' description='Description'/>
-            Categories: <input type='text' categories='Categories'/>
-          </label>
-        </form>
-        </div>
-      </header>
-    </div>
+  
   );
 }
 
