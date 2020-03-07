dc4b8d15fc7a8000771f51d9fcbb4bd3afea60e8
Hisstori <browncryan@outlook.com>
2020-03-06 19:02:52 -0500

    API call returning!

---
+++ src/Titles.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import "./App";
+// import ReactDOM from 'react-dom';
+// import axios from 'axios';
+// import { render } from '@testing-library/react';
+// const url = 'http://localhost:3000/';
+
+class Title extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    };
+  }
+  componentDidMount() {
+    if (!this.state.data) {
+      fetch("http://localhost:3280/books")
+        .then(res => res.json())
+        .then(res => {
+          this.setState({
+            data: res
+          });
+          console.log(res);
+        });
+    }
+  }
+  render() {
+    if (this.state.data) {
+      let info = this.state.data.map(item => {
+        return <div>{item.title}, [{item.categories}]</div>;
+      });
+      return <div>{info}</div>;
+    }
+    return null
+  }
+}
+export default Title;
