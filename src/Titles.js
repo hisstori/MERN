db336000d6a716b72b338676cd575593a9e5fe1a
Hisstori <browncryan@outlook.com>
2020-03-07 12:48:26 -0500

    Cleaned up code.

--- src/Titles.js
+++ src/Titles.js
@@ -5,30 +5,6 @@ import "./App";
 // import { render } from '@testing-library/react';
 
 
-// class Title extends Component {
-//   componentDidMount() {
-//     const url = 'http://localhost:3280/books';
-//     console.log(url);
-
-//     fetch(url)
-//       .then(response => response.json())
-//       .then(response => {
-//         console.log(response)
-//       })
-//       .catch(err => {
-//         console.error(err)
-//       })
-//   }
-//   render() {
-//     if (this.state.data) {
-//       let info = this.state.data.map(item => {
-//         return <div>{item.title}, [{item.categories}]</div>;
-//       });
-//       return <div>{info}</div>
-//     }
-//   }
-// }
-
 class Title extends Component {
   constructor(props) {
     super(props);
