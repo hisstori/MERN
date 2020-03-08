9da6a2d200eafdfa12c2c629d3ddcf08f83adadf
Hisstori <browncryan@outlook.com>
2020-03-08 15:33:49 -0400

    Page layout tweaks.

--- src/Titles.js
+++ src/Titles.js
@@ -27,20 +27,92 @@ class Book extends Component {
     if (this.state.data) {
       let info = this.state.data.map(item => {
         return (
-          <div>
-            {item.title}, <img src={item.thumbnailUrl} alt={item.name}
-                    />, [{item.categories}]
+          // <div>
+          //   {item.title}, <img src={item.thumbnailUrl} alt={item.name} />, [
+          //   {item.categories}]
+          // </div>
+          <div class="hero-body">
+            <div class="tile is-ancestor is-centered">
+              <div class="tile is-vertical is-8">
+                <div class="tile">
+                  <div class="tile is-parent is-">
+                    <article class="tile is-child notification is-primary">
+                      <p class="title">{item.title}</p>
+                      <p class="subtitle">
+                        {item.categories[0]}, {item.categories[1]}
+                      </p>
+                    </article>
+                    <article class="tile is-child notification is-warning">
+                      <p class="title">Authors</p>
+                      <p class="subtitle">
+                        {item.authors[0]}, {item.authors[1]}
+                      </p>
+                    </article>
+                    <div class="tile is-parent is-vertical">
+                      <article class="tile is-child notification is-info">
+                        <figure class="image">
+                          <img src={item.thumbnailUrl} />
+                        </figure>
+                      </article>
+                      <div class="tile is-parent">
+                        <article class="tile is-child notification is-danger">
+                          <p class="title">About this book...</p>
+                          <p class="subtitle">{item.longDescription}</p>
+                          <div class="content"></div>
+                        </article>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         );
       });
       return (
-        <div class="hero-body">
-          <div class="container has-text-centered is-4">
-            <div class="is-child box">
-            <h1 class="info">{info[2]}</h1>
-            </div>
-          </div>
-        </div>
+        // <div class="hero-body">
+        //   <div class="container has-text-centered is-4">
+        //     <div class="is-child box">
+        //     <h1 class="info">{info[1]}</h1>
+        //     </div>
+        //   </div>
+        // </div>
+        //=========================//
+        // <div class="hero-body">
+        //   <div class="tile is-ancestor is-centered box">
+        //     <div class="tile is-vertical is-8">
+        //       <div class="tile">
+        //         <div class="tile is-parent is-vertical">
+        //           <article class="tile is-child notification is-primary">
+        //             <p class="title">Title</p>
+        //             <p class="subtitle">{[18].title}</p>
+        //           </article>
+        //           <article class="tile is-child notification is-warning">
+        //             <p class="title">...tiles</p>
+        //             <p class="subtitle">Bottom tile</p>
+        //           </article>
+        //         </div>
+        //         <div class="tile is-parent">
+        //           <article class="tile is-child notification is-info">
+        //             <p class="title">Middle tile</p>
+        //             <p class="subtitle">With an image</p>
+        //             <figure class="image is-4by3">
+        //               <img src="https://bulma.io/images/placeholders/640x480.png" />
+        //             </figure>
+        //           </article>
+        //         </div>
+        //       </div>
+        //       <div class="tile is-parent">
+        //         <article class="tile is-child notification is-danger">
+        //           <p class="title">Wide tile</p>
+        //           <p class="subtitle">Aligned with the right tile</p>
+        //           <div class="content"></div>
+        //         </article>
+        //       </div>
+        //     </div>
+        //   </div>
+        // </div>
+        <div>{info[0]}</div>
       );
     }
     return null;
