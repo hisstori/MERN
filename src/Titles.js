22c98d48a514271dde1e2e86175989890d41c613
Hisstori <browncryan@outlook.com>
2020-03-27 21:22:14 -0400

    Test

--- src/Titles.js
+++ src/Titles.js
@@ -13,7 +13,7 @@ class Book extends Component {
   }
   componentDidMount() {
     if (!this.state.data) {
-      fetch("https://sleepy-meitner-6d618d.netlify.com/books")
+      fetch("https://infinite-fjord-09219.herokuapp.com/")
         .then(res => res.json())
         .then(res => {
           this.setState({
